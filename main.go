199de4f9a86c59f28b9eaafb13f155f396422501
Satvik Akkaraju <satvik.akkaraju@gmail.com>
2024-01-07 11:59:44 +0530

    serve html

--- main.go
+++ main.go
@@ -14,13 +14,21 @@ func main() {
 	godotenv.Load(".env")
 	fmt.Println(os.Getenv("xmx"))
 	router := gin.Default()
-	router.LoadHTMLGlob("static/*")
+	router.SetTrustedProxies([]string{"localhost"})
+	router.LoadHTMLGlob("static/*.html")
+	router.Static("/css", "./static/css")
+	router.Static("/js", "./static/js")
+	router.Static("/assets", "./static/assets")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", gin.H{
 			"title": "Satvik",
 		})
 	})
-	router.SetTrustedProxies([]string{"localhost"})
+	router.NoRoute(func(c *gin.Context) {
+		c.HTML(404, "404.html", gin.H{
+			"title": "404",
+		})
+	})
 	apiV1 := router.Group("/api/v1")
 	apiV1.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
